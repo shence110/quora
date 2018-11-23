87d29e44815d3302db740f376eb9f8851a1ac381
ce shen <shence110@vip.qq.com>
2018-11-23 10:48:03 +0800

    ssm init files

---
+++ src/main/java/tiny/quora/QuoraApplication.java
@@ -0,0 +1,12 @@
+package tiny.quora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoraApplication.class, args);
+	}
+}
